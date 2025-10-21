8a3fd1fb65fd8bb84fa4762cebd6459d99bd1264
Bukhari_Inst <bukhari414647@gmail.com>
2025-10-21 08:51:15 +0700

    Add product routes to API

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'code' => 'required',
+        ]);
+
+        $product = Product::create($validatedData);
+
+        return response()->json($product, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/product', [ProductController::class, 'index']);
+Route::post('/product-store', [ProductController::class, 'store']);
 
 Route::get('/halo', function () {
     return 'Halo, Laravel!';
