20cc034709abcdd7c6573a2347d3f0deb5d31368
Bukhari_Inst <bukhari414647@gmail.com>
2025-11-04 08:42:26 +0700

    Add vendor routes to API for resource management

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('vendors', VendorController::class);
 
 Route::get('/product', [ProductController::class, 'index']);
 Route::post('/product-store', [ProductController::class, 'store']);
